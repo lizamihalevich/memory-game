ce492382be3416801ef630bab82e3c7aed5965d7
Liza Mihalevich <lizamih99@gmail.com>
2019-01-14 17:54:11 +0300

    renaming

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -1,18 +1,18 @@
 import React from "react";
 import './style.css';
 import back from './images/back.jpg';
-import _1 from './images/_1.jpg';
-import _2 from './images/_2.jpg';
-import _3 from './images/_3.jpg';
-import _4 from './images/_4.jpg';
-import _5 from './images/_5.jpg';
-import _6 from './images/_6.jpg';
-import _7 from './images/_7.jpg';
-import _8 from './images/_8.jpg';
-import _9 from './images/_9.jpg';
-import _10 from './images/_10.jpg';
-import _11 from './images/_11.jpg';
-import _12 from './images/_12.jpg';
+import first from './images/_1.jpg';
+import second from './images/_2.jpg';
+import third from './images/_3.jpg';
+import four from './images/_4.jpg';
+import five from './images/_5.jpg';
+import six from './images/_6.jpg';
+import seven from './images/_7.jpg';
+import eight from './images/_8.jpg';
+import nine from './images/_9.jpg';
+import ten from './images/_10.jpg';
+import eleven from './images/_11.jpg';
+import twelve from './images/_12.jpg';
 import ReactCardFlip from 'react-card-flip';
 
 
@@ -32,7 +32,7 @@ export default class Card extends React.Component{
     }
 
     render() {
-        let imgs = [_1, _2, _3, _4, _5, _6, _7, _8, _9, _10, _11, _12];
+        let imgs = [first, second, third, four, five, six, seven, eight, nine, ten, eleven, twelve];
         let pic = imgs[this.props.card.image-1];
 
         return (
